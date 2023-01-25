108309d4e48dee27e683df2cadb7ca95cfffbbf3
Elieeeya <ik.slydnev@gmail.com>
2023-01-25 21:47:50 +0300

    fix project 2 sprint

--- tests.py
+++ tests.py
@@ -1,46 +1,55 @@
 class TestBooksCollector:
 
+
+
+# Изначальный словарь рейтинга (books_rating) пуст
     def test_init_book_rating_empty_true(self, cb):
         assert len(cb.books_rating) == 0
 
+
+
+# Изначальный список "Избранное" пуст
     def test_init_favorites_empty_true(self, cb):
         assert len(cb.favorites) == 0
 
+
+
+# В словарь books_rating можно добавить две книги с разным названием
     def test_add_new_books_books_added(self, cb):
         cb.add_new_book('Так говорил Заратустра'),
         cb.add_new_book('Сверхчеловек против супер-эго')
 
         assert len(cb.get_books_rating()) == 2
 
+
+
+# В словарь books_rating нельзя добавить одну и ту же книгу более одного раза
     def test_add_new_books_add_two_identical_books(self, cb):
         cb.add_new_book('Так говорил Заратустра')
         cb.add_new_book('Так говорил Заратустра')
 
         assert len(cb.get_books_rating()) == 1
 
+
+
+# Базовый рейтинг каждой книги равен единице (при добавлении в словарь рейтинга (books_rating))
     def test_add_one_book_to_books_rating_true(self, cb):
         cb.add_new_book('Так говорил Заратустра')
 
         assert cb.books_rating['Так говорил Заратустра'] == 1
 
+
+
+# Для добавленной книги можно установить рейтинг в пределах от 1 до 10
     def test_set_one_book_with_a_rating_from_1_to_11(self, cb):
         cb.add_new_book('Так говорил Заратустра')
         cb.set_book_rating('Так говорил Заратустра', 5)
 
         assert cb.books_rating['Так говорил Заратустра'] == 5
 
-    def test_set_one_book_with_a_negative_rating(self, cb):
-        cb.add_new_book('Так говорил Заратустра')
-        cb.set_book_rating('Так говорил Заратустра', -2)
 
-        assert cb.get_books_rating() != -2
 
-    def test_set_one_book_with_max_rating(self, cb):
-        cb.add_new_book('Так говорил Заратустра')
-        cb.set_book_rating('Так говорил Заратустра', 15)
-
-        assert cb.get_books_rating() != 15
-
+# Граничные значения рейтинга соблюдены
     def test_add_3_books_set_rating(self, cb):
         cb.add_new_book('Так')
         cb.add_new_book('говорил')
@@ -54,6 +63,9 @@ class TestBooksCollector:
         assert cb.get_book_rating('говорил') == 5
         assert cb.get_book_rating('Заратустра') == 10
 
+
+
+# Можно получить список книг с заданным рейтингом
     def test_add_5_books_set_rating(self, cb):
         cb.add_new_book('Так')
         cb.add_new_book('говорил')
@@ -75,6 +87,8 @@ class TestBooksCollector:
         assert len(cb.get_books_with_specific_rating(5)) == 3
         assert ['Заратустра', 'Ретеcт', 'Сколько можно'] == books_with_rating_three
 
+
+# Можно получить словарь рейтинга
     def test_get_books_rating_return_dict_with_book_with_preset_rating(self, cb):
         cb.add_new_book('Так говорил Заратустра')
         cb.set_book_rating('Так говорил Заратустра', 2)
@@ -87,30 +101,39 @@ class TestBooksCollector:
         cb.add_book_in_favorites('Так говорил Заратустра')
 
         assert 'Так говорил Заратустра' in cb.get_list_of_favorites_books()
-        assert len(cb.favorites) == 1
+        assert cb.favorites == ['Так говорил Заратустра']
 
+
+# Книга добавляется в список "Избранное"
     def test_add_book_in_favorites_book_not_in_books_rating_cant_be_added_true(self, cb):
         cb.add_book_in_favorites('Так говорил Заратустра')
 
         assert 'Так говорил Заратустра' not in cb.get_list_of_favorites_books()
         assert len(cb.favorites) == 0
 
+
+
+# Книгу можно удалить из списка "Избранное"
     def test_delete_book_from_favorites_delete_one_book(self, cb):
         cb.add_new_book('Так говорил Заратустра')
         cb.add_book_in_favorites('Так говорил Заратустра')
 
         assert 'Так говорил Заратустра' in cb.get_list_of_favorites_books()
-        assert len(cb.favorites) == 1
+        assert cb.favorites == ['Так говорил Заратустра']
 
         cb.delete_book_from_favorites('Так говорил Заратустра')
 
         assert 'Так говорил Заратустра' not in cb.get_list_of_favorites_books()
         assert len(cb.favorites) == 0
 
+
+
+# Можно получить список "Избранное"
     def test_get_list_of_favorites_books_return_list_with_one_book(self, cb):
         cb.add_new_book('Так говорил Заратустра')
         cb.add_book_in_favorites('Так говорил Заратустра')
 
         assert type(cb.favorites) == list
         assert 'Так говорил Заратустра' in cb.get_list_of_favorites_books()
-        assert len(cb.favorites) == 1
+        assert cb.favorites == ['Так говорил Заратустра']
+
